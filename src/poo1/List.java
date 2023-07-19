d9d2154e9cade174c975a757fb3a5af62e140e17
Baraka Bigega Espoir <esbarakabigega@gmail.com>
2023-07-19 09:32:53 +0200

    LIST

---
+++ src/poo1/List.java
@@ -0,0 +1,10 @@
+package poo1;
+import java.util.ArrayList;
+public class List {
+    public static void main(String[] args){
+        ArrayList<String> cars = new ArrayList<>(); 
+        cars.add("VOLVO");
+        cars.add("TX");
+        cars.add("TOYOTA");
+    }
+}
