b0e304a9167184a66cef8951c24b2bf4490b2a1b
Baraka Bigega Espoir <esbarakabigega@gmail.com>
2023-07-19 15:40:27 +0200

    jframe

--- src/poo1/Event1.java
+++ src/poo1/Event1.java
@@ -4,6 +4,10 @@ import javax.swing.JFrame;
 public class Event1 {
     public static void main(String[] args){
         JFrame fn = new JFrame();
+        fn.setSize(600,500);
+        fn.setTitle("Exercice");
+        fn.setResizable(false);
+        fn.setLocation(150, 100);
         fn.setVisible(true);
     }
 }
