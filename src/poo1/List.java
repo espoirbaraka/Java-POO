adb0877c06cf26b8dea296729e57a1376657c728
Baraka Bigega Espoir <esbarakabigega@gmail.com>
2023-07-19 09:55:30 +0200

    list

--- src/poo1/List.java
+++ src/poo1/List.java
@@ -1,13 +1,17 @@
 package poo1;
 import java.util.ArrayList;
+import java.util.Collections;
 public class List {
     public static void main(String[] args){
-        ArrayList<String> cars = new ArrayList<>(); 
+        ArrayList<String> cars = new ArrayList(); 
         cars.add("VOLVO");
         cars.add("TX");
         cars.add("TOYOTA");
         System.out.println(cars);
         cars.remove(cars.set(0,"BUS"));
+        System.out.println(cars);
+        
+        Collections.sort(cars);
         System.out.println(cars);
         for(int i = 0; i<cars.size(); i++){
             System.out.println(cars.get(i));
