d9e5ffa04725c324ca4363d283c90de5be621c53
Baraka Bigega Espoir <esbarakabigega@gmail.com>
2023-07-22 09:40:32 +0200

    other

--- src/poo1/Event1.java
+++ src/poo1/Event1.java
@@ -1,9 +1,9 @@
 package poo1;
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Event1 {
     public static void main(String[] args){
-        JFrame fn = new JFrame();
+        JFrame fn = new JFrame("Ok ok");
         fn.setSize(600,500);
         fn.setTitle("Exercice");
         fn.setResizable(false);
